fa1e82667ee3e83782fd7c0dbbd914fa371c9654
PerutkaAdam <adamperutka1@gmail.com>
2025-04-09 18:52:51 +0200

    zmena z calc.cpp na .py - robime v py, pridanie definicie add, ostatne funkcie neimplementovane

---
+++ src/calc.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    return a + b
+
+# the following functions have not been implemented yes
+def subtract(a, b):
+    raise NotImplementedError("subtract is not implemented yet.")
+
+def multiply(a, b):
+    raise NotImplementedError("multiply is not implemented yet.")
+
+def divide(a, b):
+    raise NotImplementedError("divide is not implemented yet.")
+
+def factorial(n):
+    raise NotImplementedError("factorial is not implemented yet.")
+
+def power(a, b):
+    raise NotImplementedError("power is not implemented yet.")
+
+def nth_root(a, n):
+    raise NotImplementedError("nth_root is not implemented yet.")
+
+def modulo(a, b):
+    raise NotImplementedError("modulo is not implemented yet.")
