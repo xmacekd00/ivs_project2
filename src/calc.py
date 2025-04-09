52f91c3ebe8ff3c5bde1626079183926d618ad13
PerutkaAdam <adamperutka1@gmail.com>
2025-04-09 19:23:47 +0200

    implementacia ostatnych funkcii v calc.py, testy presli vsetky

--- src/calc.py
+++ src/calc.py
@@ -12,7 +12,7 @@ def multiply(a, b):
 
 def divide(a, b):
 	if b == 0:
-		raise ZeroDivisionError("Division by zero is undefined.")
+		raise ZeroDivisionError("Division by zero is undefined!")
 	return a / b
 
 def factorial(n):
@@ -21,10 +21,14 @@ def factorial(n):
 	return math.factorial(n)
 
 def power(a, b):
-    raise NotImplementedError("power is not implemented yet.")
+	return a**b
 
 def nth_root(a, n):
-    raise NotImplementedError("nth_root is not implemented yet.")
+	if a < 0:
+		raise ValueError("Negative input not allowed for root function!")
+	return a ** (1 / n)
 
 def modulo(a, b):
-    raise NotImplementedError("modulo is not implemented yet.")
+	if b == 0:
+		raise ZeroDivisionError("Modulo by zero is undefined!")
+	return a % b
